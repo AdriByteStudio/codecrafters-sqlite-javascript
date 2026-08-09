439f4435672393d602604e8763185e92f8eb23cc
Gianfranco <25282809+ciaffo@users.noreply.github.com>
2026-08-09 23:00:50 +0200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -7,13 +7,16 @@ if (command === ".dbinfo") {
   const databaseFileHandler = await open(databaseFilePath, "r");
 
   const { buffer } = await databaseFileHandler.read({
-    length: 100,
+    length: 110,
     position: 0,
-    buffer: Buffer.alloc(100),
+    buffer: Buffer.alloc(110),
   });
 
   const pageSize = buffer.readUInt16BE(16); // page size is 2 bytes starting at offset 16
+  const numberOfCells = buffer.readUInt16BE(103); // page 1 b-tree header begins at offset 100; cells count is at relative offset 3
+
   console.log(`database page size: ${pageSize}`);
+  console.log(`number of tables: ${numberOfCells}`);
 } else {
   throw `Unknown command ${command}`;
 }
